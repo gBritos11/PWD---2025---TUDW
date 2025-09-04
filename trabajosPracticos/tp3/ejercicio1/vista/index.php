8063f83a90a2de452cbbbd31abcb304ffcc21d29
gBritos11 <gabriel.britos.epet20@gmail.com>
2025-09-04 18:24:59 -0300

    estilos tp3

--- trabajosPracticos/tp3/ejercicio1/vista/index.php
+++ trabajosPracticos/tp3/ejercicio1/vista/index.php
@@ -9,23 +9,36 @@
             include_once '../../../includes/head.php';
         ?>
     </head>
-    <body>
-        
-        <form id="formSubirArchivo" name="formSubirArchivo" action="../control/controladorFormSubirArchivo.php" method="post" enctype="multipart/form-data" novalidate>
-            <fieldset>
-                <legend>Subir Archivo</legend>
-                <label for="archivo">Subir Archivo</label>
-                <input type="file" name="archivo" id="archivo">
-                <input type="submit" value="Enviar">
-            </fieldset>
-        </form>
+    <body class="bg-secondary">
+        <div class="container d-flex justify-content-center align-items-center vh-100">
+            <form id="formSubirArchivo" name="formSubirArchivo" action="../control/controladorFormSubirArchivo.php" method="post" enctype="multipart/form-data" novalidate>
+                <fieldset class="border p-4 rounded shadow bg-light">
+                    <div class="p-2 fw-bold">
+                        <i class="bi bi-file me-2"></i>Subir archivo
+                    </div>
+
+                    <div class="mb-2 mx-auto">
+                        <label class="form-label" for="archivo">Subir Archivo</label>
+                        <input class="form-control" type="file" name="archivo" id="archivo">
+                    </div>
+
+                    <div class="d-grid mb-2">
+                        <input class="btn btn-success" type="submit" name="submit" value="Enviar">
+                    </div>
+                </fieldset>
+            </form>
+        </div>
 
         <!-- Obtengo el resultado -->
         <?php
             if (isset($_SESSION['resultado'])) {
-                echo '<a href="'.$_SESSION['resultado'].'">Archivo subido</a>';
+                echo '
+                    <div class="alert alert-info text-center">
+                        <a class="link-primary me-2" href="'.$_SESSION['resultado'].'">Archivo subido</a>
+                    </div>
+                ';
             }elseif(isset($_SESSION['mensaje'])){
-                echo '<p>'.$_SESSION['mensaje'].'</p>';
+                echo '<div class="alert alert-info text-center">'.$_SESSION['mensaje'].'</div>';
             }
 
             unset($_SESSION['resultado'], $_SESSION['mensaje']); // Limpio para que no quede persistente
--- trabajosPracticos/tp3/ejercicio2/vista/index.php
+++ trabajosPracticos/tp3/ejercicio2/vista/index.php
@@ -9,28 +9,37 @@
             include_once '../../../includes/head.php';
         ?>
     </head>
-    <body>
-        
-        <form id="formSubirArchivoTXT" name="formSubirArchivoTXT" action="../control/controladorFormSubirArchivoTXT.php" method="post" enctype="multipart/form-data" novalidate>
-            <fieldset>
-                <legend>Subir Archivo TXT</legend>
-                <label for="archivo">Subir Archivo</label>
-                <input type="file" name="archivo" id="archivo">
-                <input type="submit" value="Enviar">
-            </fieldset>
-        </form>
+    <body class="bg-secondary">
+        <div class="container d-flex justify-content-center align-items-center vh-100">
+            <form id="formSubirArchivoTXT" name="formSubirArchivoTXT" action="../control/controladorFormSubirArchivoTXT.php" method="post" enctype="multipart/form-data" novalidate>
+                <fieldset class="border p-4 rounded shadow bg-light">
+                    <div class="p-2 fw-bold">
+                        <i class="bi bi-file-earmark-text me-2"></i>Subir archivo TXT
+                    </div>
+                    <div class="mb-2 mx-auto">
+                        <label class="form-label" for="archivo">Subir Archivo</label>
+                        <input class="form-control" type="file" name="archivo" id="archivo">
+                    </div>
+                    <div class="d-grid mb-2">
+                        <input class="btn btn-success" type="submit" name="submit" value="Enviar">
+                    </div>
+                </fieldset>
+            </form>
+        </div>
 
         <!-- Obtengo el resultado -->
         <?php
             if (isset($_SESSION['resultado'])) {
-                echo '<label for="contenido">Contenido del archivo:</label>';
                 echo '
-                    <textarea id="contenido" rows="10" cols="80" readonly>'
-                    . htmlspecialchars($_SESSION['resultado']) .
-                    '</textarea>
+                    <div class="mb-3 class="alert alert-info text-center">
+                        <label for="contenido" class="form-label">Contenido del archivo: </label>
+                        <textarea id="contenido" rows="10" cols="80" readonly class="form-control bg-light">
+                        '.htmlspecialchars($_SESSION['resultado']).'
+                        </textarea>
+                    </div>
                 ';
             }elseif(isset($_SESSION['mensaje'])){
-                echo '<p>'.$_SESSION['mensaje'].'</p>';
+                echo '<div class="alert alert-info text-center">'.$_SESSION['mensaje'].'</div>';
             }
 
             unset($_SESSION['resultado'], $_SESSION['mensaje']); // Limpio para que no quede persistente
